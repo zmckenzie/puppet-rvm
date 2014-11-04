af954046c2cc410174f78ffffa9de2dc9d8dd08e
Zachary Mckenzie <zachary.mckenzie@agapered.com>
2014-11-04 15:42:36 -0600

    Update system.pp

--- manifests/system.pp
+++ manifests/system.pp
@@ -3,6 +3,7 @@ class rvm::system($version='latest') {
     path    => '/usr/bin:/usr/sbin:/bin',
     command => "bash -c '/usr/bin/curl -s https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer ; \
                 chmod +x /tmp/rvm-installer ; \
+                gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3 && \
                 rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${version}  --autolibs=enable; \
                 rm /tmp/rvm-installer'",
     creates => '/usr/local/rvm/bin/rvm',
